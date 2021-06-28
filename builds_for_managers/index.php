b71c6b8c81f02e0ce0d4fa80da4e0e6444be3fad
Nagesh Raghupatruni <37229952+nageshrag@users.noreply.github.com>
2021-06-28 12:19:27 +0530

    Update index.php

--- builds_for_managers/index.php
+++ builds_for_managers/index.php
@@ -1,9 +1,12 @@
 
 <?php
- throw new Exception("Error occured");
+ 
  $result="";
 
  foreach ($_POST as $key => $value) {
+   if($value=='error') {
+     throw new Exception("Error occured");
+   }
    $result.=$value. "\n";
  }
   file_put_contents('data.txt',$result);
