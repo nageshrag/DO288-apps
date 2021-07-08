f82e87bc20fe41e55b1f72d352d229b28cbd31f5
Nagesh Raghupatruni <37229952+nageshrag@users.noreply.github.com>
2021-07-08 14:10:27 +0530

    Create quotes.sql

---
+++ post-commit/quotes.sql
@@ -0,0 +1,13 @@
+create table quote (
+     id int NOT NULL AUTO_INCREMENT,
+     msg TINYTEXT,
+     PRIMARY KEY (id)
+  );
+
+
+insert into quote values(1,'\"Love For All, Hatred For None.\"');
+insert into quote values(2,'\"Change the world by being yourself.\"');
+insert into quote values(3,'\"Every moment is a fresh beginning.\"');
+insert into quote values(4,'\"Never regret anything that made you smile.\"');
+insert into quote values(5,'\"Die with memories, not dreams.\"');
+insert into quote values(6,'\"Aspire to inspire before we expire. \"');
