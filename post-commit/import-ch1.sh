deae62f142062b23d84dbfb7e167f1b50dd41603
Nagesh Raghupatruni <37229952+nageshrag@users.noreply.github.com>
2021-07-08 14:09:58 +0530

    Create import-ch1.sh

---
+++ post-commit/import-ch1.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+HOOK_RETRIES=5
+HOOK_SLEEP=1
+MYSQL_SERVICE_HOST="mysql"
+MYSQL_USER="test"
+MYSQL_PASSWORD="redhat"
+MYSQL_DATABASE="testdb"
+echo 'Downloading SQL script that initializes the database...'
+curl -0 -s https://raw.githubusercontent.com/nageshrag/DO288-apps/master/post-commit/quotes.sql -o /tmp/quotes.sql
+echo "Trying $HOOK_RETRIES times, sleeping $HOOK_SLEEP sec between tries:"
+while [ "$HOOK_RETRIES" != 0 ]; do
+  echo -n 'Checking if MySQL is up...'
+  if mysqlshow -h$MYSQL_SERVICE_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -P3306 $MYSQL_DATABASE &>/dev/null
+  then
+    echo 'Database is up'
+    break
+  else
+    echo 'Database is down'
+    # Sleep to wait for the MySQL pod to be ready
+    sleep $HOOK_SLEEP
+  fi
+  let HOOK_RETRIES=HOOK_RETRIES-1
+done
+if [ "$HOOK_RETRIES" = 0 ]; then
+  echo 'Too many tries, giving up'
+  exit 1
+fi
+echo "list files"
+for entry in "/tmp/"/*
+do
+  echo "$entry"
+done
+
+# Run the SQL script
+if mysql -h$MYSQL_SERVICE_HOST -u$MYSQL_USER -p$MYSQL_PASSWORD -P3306 $MYSQL_DATABASE < /tmp/quotes.sql
+then
+  echo 'Database initialized successfully'
+else
+  echo 'Failed to initialize database'
+exit 2
+fi
